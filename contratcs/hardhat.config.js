774915899b61c165fc8036f78388424fde4d5d17
Marybeee <maria.behappy.mb@gmail.com>
2022-07-25 07:10:25 +0200

    setup

---
+++ contratcs/hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { API_URL_POLYGON, PRIVATE_KEY, API_URL_ETHERSCAN } = process.env; 
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    polygonMumbai: {
+      url: API_URL_POLYGON,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: API_URL_ETHERSCAN,
+  }
+};
