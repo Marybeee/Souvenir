80bde4fa15a1dbe9cd233a6b08accd1972d8b265
Marybeee <maria.behappy.mb@gmail.com>
2022-07-26 06:44:03 +0200

    Commenting COnversations

--- contratcs/contracts/Conversations.sol
+++ contratcs/contracts/Conversations.sol
@@ -5,11 +5,15 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Conversations {
     
+    // using counters to ensure only incrementing by 1
     using Counters for Counters.Counter;
-    Counters.Counter public conversationCounter;
+    // setting conversationCounter to be the Counter
+    Counters.Counter private conversationCounter;
+    
+    // Event for the succesful registration of a new Conversastion.
+    event NewConversationRegistered (Conversation _newConversation);
 
-    event NewConversationRegistered (uint indexed conversationId, bytes32 _conversationName, address personAWallet, address personBWallet);
-
+    // struct of the Conversation with sepcific information needed to mint 
     struct Conversation {
         uint conversationId;
         bytes32 conversationName;
@@ -17,22 +21,24 @@ contract Conversations {
         address personBWallet;
     }
 
+    // array of all the Conversations
     Conversation[] public conversations;
 
-    // stuct Conversation with conversationName, conversationId, personA and personB
-    // mapping with humans and conversations
-
+    // Function to register a new Conversation based on input variables.
     function registerConversation(bytes32 _conversationName, address _personAWallet, address _personBWallet) external  {
+        // increment the conversation counter
         conversationCounter.increment();
+        // set the new conversation with its specific values
         Conversation memory _newConversation = Conversation({
             conversationId: conversationCounter._value,
             conversationName: _conversationName,
             personAWallet: _personAWallet,
             personBWallet: _personBWallet
         });
+        // add the conversation to the conversations array
         conversations.push(_newConversation);
-       
-        emit NewConversationRegistered (conversationCounter._value, _conversationName, _personAWallet, _personBWallet);
+        // emit the new Conversation registered
+        emit NewConversationRegistered (_newConversation);
     }
 
 }
