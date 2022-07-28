a3eb2e9774e623d7abc6a6e63e690f91fb6c6d8f
Sahil jain <sahil13jain@gmail.com>
2022-07-28 16:53:13 +0530

    images added in gallery

--- frontend/src/helpers/MenuList.js
+++ frontend/src/helpers/MenuList.js
@@ -1,30 +1,30 @@
-import first from "../assets/bannerimage.jpeg";
-import second from "../assets/bannerimage.jpeg";
-import third from "../assets/bannerimage.jpeg";
-import fourth from "../assets/bannerimage.jpeg";
-import fifth from "../assets/bannerimage.jpeg";
-import sixth from "../assets/bannerimage.jpeg";
+import first from "../assets/firstimage.png";
+import second from "../assets/secondimage.png";
+import third from "../assets/thirdimage.png";
+import fourth from "../assets/fourthimage.png";
+import fifth from "../assets/fifthimage.png";
+import sixth from "../assets/sixthimage.png";
 
 export const MenuList = [
   {
-    name: "First Souvenir",
+    name: "26 July, 2022",
     image: first,
-    price: "Something about the image",
+    price: "A painting of a young woman, around the age of 18. She is standing in a field of tall grass, with mountains in the background",
   },
   {
     name: "Second Souvenir",
     image: second,
-    price: "Something about the image",
+    price: "A scene of a music festival in the mountains ,with people of all ages and backgrounds dancing and enjoying the music",
   },
   {
     name: "Third Souvenir",
     image: third,
-    price: "Something about the image",
+    price: "A black and white portrait drawing of a nature-loving and rebellious teenage girl set in a mountain landscape.",
   },
   {
     name: "Fourth Souvenir",
     image: fourth,
-    price: "Something about the image",
+    price: "A painting of a girl in a black dress with chunky",
   },
   {
     name: "5 Souvenir",
