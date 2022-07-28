8f0b3c98bea513232d27f7d86f1a69af875efba9
Marybeee <maria.behappy.mb@gmail.com>
2022-07-28 08:14:53 +0200

    added deployment script for the contracts

--- contratcs/scripts/deploy.js
+++ contratcs/scripts/deploy.js
@@ -4,21 +4,18 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const ConversationNFT = await ethers.getContractFactory("ConversationNFT");
+  const conversationNFT = await ConversationNFT.deploy("Souvenir","SUV");
+  console.log("ConversationNFT is deployed on Address:", conversationNFT.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  const ConversationNFTHandler = await ethers.getContractFactory("ConversationNFTHandler");
+  const conversationNFTHandler = await ConversationNFTHandler.deploy(conversationNFT.address);
+  console.log("ConversationNFTHandler deployed to :", conversationNFTHandler.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
